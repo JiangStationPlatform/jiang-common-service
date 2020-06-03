f1514660ab8bc62e5ea5bcedb35464dcb0414017
jshk123 <jshk123@qq.om>
2020-06-03 09:46:06 +0800

    update

--- src/main/java/cn/jiang/station/platform/common/mapper/TbPostsPostMapper.java
+++ src/main/java/cn/jiang/station/platform/common/mapper/TbPostsPostMapper.java
@@ -2,7 +2,10 @@ package cn.jiang.station.platform.common.mapper;
 
 
 import cn.jiang.station.platform.common.domain.TbPostsPost;
+import cn.jiang.station.platform.common.utils.RedisCache;
+import org.apache.ibatis.annotations.CacheNamespace;
 import tk.mybatis.mapper.MyMapper;
 
+@CacheNamespace(implementation = RedisCache.class)
 public interface TbPostsPostMapper extends MyMapper<TbPostsPost> {
 }
--- src/main/java/cn/jiang/station/platform/common/mapper/TbSysUserMapper.java
+++ src/main/java/cn/jiang/station/platform/common/mapper/TbSysUserMapper.java
@@ -2,7 +2,10 @@ package cn.jiang.station.platform.common.mapper;
 
 
 import cn.jiang.station.platform.common.domain.TbSysUser;
+import cn.jiang.station.platform.common.utils.RedisCache;
+import org.apache.ibatis.annotations.CacheNamespace;
 import tk.mybatis.mapper.MyMapper;
 
+@CacheNamespace(implementation = RedisCache.class)
 public interface TbSysUserMapper extends MyMapper<TbSysUser> {
 }
